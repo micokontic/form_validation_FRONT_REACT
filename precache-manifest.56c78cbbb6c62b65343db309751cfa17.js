f73818b5f90a2291c9379823c353a61add279671
Mico Kontic <micokontic@gmail.com>
2020-10-12 15:58:21 +0200

    Updates

---
+++ precache-manifest.56c78cbbb6c62b65343db309751cfa17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef57a8588e02cfc049dc4617540ee90",
+    "url": "/micokontic/form_validation_FRONT_REACT/index.html"
+  },
+  {
+    "revision": "4396aea1811de0e6eaff",
+    "url": "/micokontic/form_validation_FRONT_REACT/static/css/main.d3054c09.chunk.css"
+  },
+  {
+    "revision": "13cb0c463328081ce99e",
+    "url": "/micokontic/form_validation_FRONT_REACT/static/js/2.c1a10aca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/micokontic/form_validation_FRONT_REACT/static/js/2.c1a10aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4396aea1811de0e6eaff",
+    "url": "/micokontic/form_validation_FRONT_REACT/static/js/main.3c7fc3b9.chunk.js"
+  },
+  {
+    "revision": "8ab9a414792b0312326b",
+    "url": "/micokontic/form_validation_FRONT_REACT/static/js/runtime-main.7207fb40.js"
+  }
+]);
